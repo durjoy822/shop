3c873ee73531147a6c9f5cd29d2669c33147857a
durjoy <nrdurjoy822@gmail.com>
2023-10-10 22:58:20 +0600

    dashboard show

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
-Route::get('/home', function () {
-    return view('welcome ' );
-});
-Route::get('/about', function () {
-    return view('welcome ' );
+Route::get('/dashboard', function () {
+    return view('admin/dashboard');
 });
 
