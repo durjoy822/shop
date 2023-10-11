21d19fee20cf6befeba4207f10cd0983f31acd9d
durjoy <nrdurjoy822@gmail.com>
2023-10-12 00:27:18 +0600

    complet user create page

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
 
@@ -9,6 +10,7 @@ use App\Http\Controllers\Admin\UserController;
 Route::get('/dashboard', function () {
     return view('admin/dashboard');
 });
+Route::get('/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
 
 Route::controller(UserController::class)->group(function () {
     Route::get('/user', 'user')->name('user');
