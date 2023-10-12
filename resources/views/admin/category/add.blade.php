1ff8af0ed98ca433a32a8910a5b225ad4162efb1
durjoy <nrdurjoy822@gmail.com>
2023-10-12 23:25:01 +0600

    category desigin

---
+++ resources/views/admin/category/add.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.layout.master')
+@section('title')
+categories create
+@endsection
+@section('content')
+<div class="container-fluid" id="container-wrapper">
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Categories Create</h1>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
+      <li class="breadcrumb-item"><a href="./">categories</a></li>
+      <li class="breadcrumb-item active" aria-current="page">categories create</li>
+    </ol>
+</div>
+    <div class="row " >
+        <div class="col-lg-8 offset-lg-2">
+        <!-- Form Basic -->
+        <div class="card mb-4">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <h6 class="m-0 font-weight-bold text-primary">Categories create </h6>
+            </div>
+            <div class="card-body">
+            <form>
+                <div class="form-group">
+                <label for="name">Category Name</label>
+                <input type="text" class="form-control"  name="name" id="name"
+                    placeholder="Enter category name">
+                </div>
+                    <div class="row">
+                        <div class="col-lg-8 pb-2">
+                            <input class="form-control " type="file" id="file-input">
+                        </div>
+                        <div class="col-lg-4 ">
+                            <img src=""  alt="" class="col-8"  id="image-previewer"  >
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+            </form>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/admin/layout/head.blade.php
+++ resources/views/admin/layout/head.blade.php
@@ -4,9 +4,11 @@
 <meta name="description" content="">
 <meta name="author" content="">
 <link href="{{asset('admin')}}/img/logo/logo.png" rel="icon">
-<title>RuangAdmin - Dashboard</title>
+<title>RuangAdmin - @yield('title')</title>
 <link href="{{asset('admin')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 <link href="{{asset('admin')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 <link href="{{asset('admin')}}/css/ruang-admin.min.css" rel="stylesheet">
 
  <link href="{{asset('admin')}}/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+
