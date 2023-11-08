47db10593da7ca352125f009f061f627503416fc
durjoy <nrdurjoy822@gmail.com>
2023-11-08 22:12:14 +0600

    google login complete

--- routes/settings.php
+++ routes/settings.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\OptimizeController;
-
-
+use App\Http\Controllers\Admin\SocialLoginController;
 
 Route::resource('/roles',RoleController::class);
 
@@ -12,3 +11,11 @@ Route::get('/optimize',[OptimizeController::class,'optimize'])->name('optimize')
 Route::get('/optimize/clear',[OptimizeController::class,'optimizeClear'])->name('optimize.clear');
 Route::get('/migrate',[OptimizeController::class,'runMigrate'])->name('artisan.migrate');
 Route::get('/migrate/fresh/seed',[OptimizeController::class,'migrateFreshSeed'])->name('artisan.migrate.fresh.seed');
+
+
+Route::controller(SocialLoginController::class)->group(function () {
+    Route::get('/auth/google', 'LoginGoogle')->name('google');
+    Route::any('/auth/google/callback', 'GoogleCallback')->name('callback');
+
+
+});
